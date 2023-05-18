a5aa24b316d0ae547861fa949cb2872ef3a150ef
imanishi.naoki <imanishi53web@gmail.com>
2023-05-18 21:11:33 +0900

    ヘッダー完成

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 //megamenu1階層目
 (() => {
   //
+  const trigger = document.querySelectorAll('.hamburger');
+  const target = document.querySelectorAll('.navi');
+  const overlay = document.querySelectorAll('.navi-overlay');
+
+  trigger[0].addEventListener('click', () => {
+    target[0].classList.toggle('is_active');
+  });
+
+  overlay[0].addEventListener('click', () => {
+    target[0].classList.toggle('is_active');
+  });
 })();
