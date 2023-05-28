b3a140c9598bd28c9f9ad021286076c8b6410c47
imanishi.naoki <imanishi53web@gmail.com>
2023-05-28 18:21:11 +0900

    TOP完成

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@
   const trigger = document.querySelectorAll('.hamburger');
   const target = document.querySelectorAll('.navi');
   const overlay = document.querySelectorAll('.navi-overlay');
+  const container = document.querySelectorAll('.header div.container');
 
-  trigger[0].addEventListener('click', () => {
+  container[0].addEventListener('click', (event) => {
+    console.log(event);
+    event.stopPropagation();
+  });
+  trigger[0].addEventListener('click', (event) => {
     target[0].classList.toggle('is_active');
+    event.stopPropagation();
   });
 
-  overlay[0].addEventListener('click', () => {
+  overlay[0].addEventListener('click', (event) => {
     target[0].classList.toggle('is_active');
+    event.stopPropagation();
   });
 })();
