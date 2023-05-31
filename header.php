dc7142838746240e0263dfc3846fcd43fc4d9353
imanishi.naoki <imanishi53web@gmail.com>
2023-06-01 00:57:13 +0900

    メタタグ最適化

--- header.php
+++ header.php
@@ -12,6 +12,10 @@
     <meta property="og:title" content="埼玉県のWEB制作会社 | In-Production" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="革新的な未来を創造する、イノベーションを提供します。" />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="埼玉県のWEB制作会社 | In-Production">
+    <meta name="twitter:description" content="革新的な未来を創造する、イノベーションを提供します。">
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/hero.jpg">
     <?php }?>
     <?php if( is_page( "about" ) ){ ?>
     <title>会社概要 | 埼玉県のWEB制作会社 | In-Production</title>
@@ -20,6 +24,10 @@
     <meta property="og:title" content="会社概要 | 埼玉県のWEB制作会社 | In-Production" />
     <meta property="og:type" content="article" />
     <meta property="og:description" content="弊社はWebデザイン・制作・運用を通じて、お客様のビジネスの成長を支援しています。" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="会社概要 | 埼玉県のWEB制作会社 | In-Production">
+    <meta name="twitter:description" content="弊社はWebデザイン・制作・運用を通じて、お客様のビジネスの成長を支援しています。">
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/hero.jpg">
     <?php }?>
     <?php if( is_page( "service" ) ){ ?>
     <title>サービス | 埼玉県のWEB制作会社 | In-Production</title>
@@ -28,6 +36,10 @@
     <meta property="og:title" content="サービス | 埼玉県のWEB制作会社 | In-Production" />
     <meta property="og:type" content="article" />
     <meta property="og:description" content="デザイン・コーディング・CMS構築・ドメイン・ホスティング等、一般的なWebサイト制作に関する全ての業務を提供しています。" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="サービス | 埼玉県のWEB制作会社 | In-Production">
+    <meta name="twitter:description" content="デザイン・コーディング・CMS構築・ドメイン・ホスティング等、一般的なWebサイト制作に関する全ての業務を提供しています。">
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/hero.jpg">
     <?php }?>
     <?php if( is_page( "price" ) ){ ?>
     <title>料金プラン | 埼玉県のWEB制作会社 | In-Production</title>
@@ -36,6 +48,10 @@
     <meta property="og:title" content="料金プラン | 埼玉県のWEB制作会社 | In-Production" />
     <meta property="og:type" content="article" />
     <meta property="og:description" content="お客様が弊社にご依頼の際にはシンプルプラン、スタンダードプラン、フルオーダープランから選んでいただきます。" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="料金プラン | 埼玉県のWEB制作会社 | In-Production">
+    <meta name="twitter:description" content="お客様が弊社にご依頼の際にはシンプルプラン、スタンダードプラン、フルオーダープランから選んでいただきます。">
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/hero.jpg">
     <?php }?>
     <?php if( is_page( "contact" ) ){ ?>
     <title>お問い合わせ | 埼玉県のWEB制作会社 | In-Production</title>
@@ -44,6 +60,10 @@
     <meta property="og:title" content="お問い合せ | 埼玉県のWEB制作会社 | In-Production" />
     <meta property="og:type" content="article" />
     <meta property="og:description" content="お客さまの要望にフレキシブルにご対応させて頂きます。" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="お問い合せ | 埼玉県のWEB制作会社 | In-Production">
+    <meta name="twitter:description" content="おお客さまの要望にフレキシブルにご対応させて頂きます。">
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/hero.jpg">
     <?php }?>
     <?php if( is_page( "thanks" ) ){ ?>
     <title>お問合せを受け付けました | 埼玉県のWEB制作会社 | In-Production</title>
@@ -52,7 +72,12 @@
     <meta property="og:title" content="お問合せを受け付けました | 埼玉県のWEB制作会社 | In-Production" />
     <meta property="og:type" content="article" />
     <meta property="og:description" content="お送り頂きました内容を確認の上、3営業日以内に折り返しご連絡させて頂きます。" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="お問合せを受け付けました | 埼玉県のWEB制作会社 | In-Production">
+    <meta name="twitter:description" content="お送り頂きました内容を確認の上、3営業日以内に折り返しご連絡させて頂きます。">
+    <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/hero.jpg">
     <?php }?>
+    <meta name="twitter:site" content="@yumejanai60">
     <link href="<?php echo get_template_directory_uri(); ?>/css/reset.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
